0920db9e6a6aa7b8f039d2a5af6dd07e4143a6e0
Augusto Soncini <augusto.soncini@globant.com>
2018-05-24 15:25:29 -0300

    AS - Now using UserLocalServiceUtil

--- liferay-plugins-sdk-7.0/portlets/ClubTechPortlet/src/main/java/com/WareTech/ClubTech/views/PlayerListView.java
+++ liferay-plugins-sdk-7.0/portlets/ClubTechPortlet/src/main/java/com/WareTech/ClubTech/views/PlayerListView.java
@@ -1,31 +1,42 @@
 package com.WareTech.ClubTech.views;
 
+import java.security.Principal;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
 
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
 
 @ManagedBean
 public class PlayerListView {
 
+	final static Logger LOGGER = Logger.getLogger(PlayerListView.class);
+	
 	public List<User> getUsers() {
-		
+		LOGGER.info("getUsers()");
 		List<User> userList = UserLocalServiceUtil.getUsers(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 		
 		return userList;
 	}
 	
 	public User getUser() {
+		LOGGER.info("getUser()");
 		try {
-			HttpServletRequest httpServletRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-			return PortalUtil.getUser(httpServletRequest);
+			Principal principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+			
+			if (principal == null) {
+				return null;
+			}
+			
+			String userId = principal.getName();
+			return UserLocalServiceUtil.getUser(new Long(userId));
 		} catch (Exception exception) {
+			LOGGER.error(exception);
 			return null;
 		}
 	}
